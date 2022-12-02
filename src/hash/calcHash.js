f834d6c8f9dc7af490b7b7b06ab8c8a7d65265ed
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2022-12-02 21:42:13 +0300

    feat: add solve for calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'node:crypto';
+import fs from 'node:fs/promises';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+  const PATH = './files/fileToCalculateHashFor.txt';
+
+  try {
+    const content = await fs.readFile(new URL(PATH, import.meta.url), { encoding: 'utf8' });
+    console.log(createHash('sha256').update(content).digest('hex'));
+  } catch (e) {
+    throw new Error('Operation failed')
+  }
 };
 
 await calculateHash();
