0c8b48608365f2f810a0a8a0cee838849b913e92
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2022-12-03 18:50:24 +0300

    feat: add solve for streams:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+import process from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const PATH = './files/fileToRead.txt';
+
+  const readableStream = fs.createReadStream(new URL(PATH, import.meta.url), 'utf8');
+  readableStream.on('data', (chunk) => {
+    process.stdout.write(chunk + '\n');
+})
 };
 
 await read();
