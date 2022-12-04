6dbccbc35212dc0bbddfcf4370f818f88845ce64
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2022-12-04 20:42:08 +0300

    feat: add solve for cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from 'node:child_process';
 
-spawnChildProcess();
+const spawnChildProcess = async (args) => {
+  const CHILD_PATH = './files/script.js';
+
+  const child = fork(new URL(CHILD_PATH, import.meta.url), args);
+  child.on('error', err => process.stdout.write('Error creating child process: ' + '\n' + err));
+  child.on('spawn', () => process.stdout.write('Child process creating succeeded.\nWrite something and finish typing by pressing <ENTER>.\nIf you want to close the child process please typing CLOSE and press <ENTER>.\n\n'));
+  child.on('close', (code) => process.stdout.write(`\nChild process exited.\nChild process send exit code: ${code}`));
+}
+
+spawnChildProcess(['Vasya', 123, true, 'one million']);
