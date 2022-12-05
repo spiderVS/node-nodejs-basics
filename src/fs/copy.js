46c1bd4e4bc81715fc91c36878b492757434117a
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2022-12-05 22:10:22 +0300

    feat: add additional solve for fs:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,16 +1,42 @@
 import fs from 'node:fs/promises';
 
 const copy = async () => {
-  const DIRNAME = 'files';
-  const COPY_SUFFIX = '_copy'
+  const SOURCE_DIRNAME = 'files';
+  const COPY_SUFFIX = '_copy';
+  const DESTINATION_DIRNAME = `${SOURCE_DIRNAME}${COPY_SUFFIX}`;
 
-  const getURL = (dirName) => new URL(`./${dirName}/`, import.meta.url);
+  const getURL = (dirName, fileName = '') => new URL(`./${dirName}/${fileName}`, import.meta.url);
 
-  try {
-    await fs.cp(getURL(DIRNAME), getURL(DIRNAME + COPY_SUFFIX), { recursive: true, force: false, errorOnExist: true });
-  } catch (e) {
-    throw new Error('FS operation failed');
+  // Solve var.1 - Simple use experimental function fsPromises.cp(src, dest[, options])
+  /*
+    try {
+      await fs.cp(getURL(SOURCE_DIRNAME), getURL(DESTINATION_DIRNAME), { recursive: true, force: false, errorOnExist: true });
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
+  */
+
+  // Solve var.2 - Recursive copy folder with internal folders and files
+  const copyFolder = async (from, to) => {
+    try {
+      let objects = await fs.readdir(getURL(from), { withFileTypes: true });
+      await fs.mkdir(getURL(to), { recursive: false });
+
+      for (const obj of objects) {
+        let fromURL = getURL(from, obj.name);
+        let toURL = getURL(to, obj.name);
+        if (obj.isDirectory()) {
+          await copyFolder(fromURL, toURL)
+        } else {
+          await fs.copyFile(fromURL, toURL);
+        }
+      }
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
   }
+  copyFolder(SOURCE_DIRNAME, DESTINATION_DIRNAME);
+
 };
 
 copy();
